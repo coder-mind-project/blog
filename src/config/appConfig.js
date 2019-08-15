86b427cdb4c2799e2662f283c4fbf4774e8eb0b8
Allan Wanderley Alves <awallan259@gmail.com>
2019-08-15 02:48:23 -0300

    URL routes configured

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,21 +1,21 @@
 export const version = '1.0.2'
 export const build = 'Beta'
 
-export const url = 'http://localhost:3000'
+export const url = 'https://codermind.com.br'
 
 // API Gestão | Management
-export const api_cm_management = 'http://localhost:3001'
+export const api_cm_management = 'https://cm-gestao-v1.allanalves23.top'
 
 // Apps API
-export const api_cm_web_service = 'http://localhost:3010'
+export const api_cm_web_service = 'https://cm-api-v1.allanalves23.top'
 
 /*
     Produção | Production
 
-    export const url = ''
+    export const url = 'https://codermind.com.br'
 
-    export const api_cm_management = ''
-    export const api_cm_web_service = ''
+    export const api_cm_management = 'https://cm-gestao-v1.allanalves23.top'
+    export const api_cm_web_service = 'https://cm-api-v1.allanalves23.top'
 
 
     ==========================================================
