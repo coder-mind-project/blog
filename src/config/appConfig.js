06a0bfab77837a10b502a366b14a420abab7c285
Allan Wanderley Alves <awallan259@gmail.com>
2019-09-20 12:27:54 -0300

    Merge com a versão 1.0.7

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,15 +1,15 @@
 export const version = '1.0.7'
 export const build = 'Beta'
 
-export const url = 'http://localhost:3000'
+export const url = 'https://codermind.com.br'
 
 export const ipify = 'https://api.ipify.org?format=json'
 
 // API Gestão | Management
-export const api_cm_management = 'http://localhost:3001'
+export const api_cm_management = 'https://cm-gestao-v1.allanalves23.top'
 
 // Apps API
-export const api_cm_web_service = 'http://localhost:3010'
+export const api_cm_web_service = 'https://cm-api-v1.allanalves23.top'
 
 /*
     Produção | Production
