917498e3bb4489c1cc8a59eb7f9ca3e4c442e2d5
Allan Wanerley Alves <awallan259@gmail.com>
2020-12-13 20:36:11 -0300

    chore: Refactored style code

--- src/components/styles/ArticleBox.js
+++ src/components/styles/ArticleBox.js
@@ -1,61 +1,61 @@
-export const styles = theme => ({
-    logoArticle: {
-        maxHeight: '180px',
-    },
-    articleBox: {
-        display: 'flex',
-        marginTop: '20px',
-        marginBottom: '20px',
-        flexWrap: 'wrap',
-    },
-    articleBoxXs: {
-        display: 'flex',
-        marginTop: '20px',
-        marginBottom: '20px',
-        flexWrap: 'wrap',
-        margin: '10px', 
-        padding: '20px',
-        boxShadow: '0px 2px 4px #ccc'
-    },
-    articleImg:{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    articleContent: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        height: '100%'
-    },
-    articleContentXs: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title:{
-        textTransform: 'uppercase',
-    },
-    link: {
-        textDecoration: 'underline',
-        color: '#8a05be',
-    },
-    linkXs:{
-        textDecoration: 'underline',
-        color: '#8a05be',
-        display: 'flex',
-        justifyContent: 'center',
-        padding: '15px',
-    },
-    textXs:{
-        paddingTop: '50px',
-        paddingLeft: '50px',
-        paddingRight: '50px',
-        paddingBottom: '10px',
-        display: 'flex',
-        justifyContent: 'center',
-    }
-})
+export const styles = (theme) => ({
+  logoArticle: {
+    maxHeight: '180px',
+  },
+  articleBox: {
+    display: 'flex',
+    marginTop: '20px',
+    marginBottom: '20px',
+    flexWrap: 'wrap',
+  },
+  articleBoxXs: {
+    display: 'flex',
+    marginTop: '20px',
+    marginBottom: '20px',
+    flexWrap: 'wrap',
+    margin: '10px',
+    padding: '20px',
+    boxShadow: '0px 2px 4px #ccc',
+  },
+  articleImg: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  articleContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
+  },
+  articleContentXs: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textTransform: 'uppercase',
+  },
+  link: {
+    textDecoration: 'underline',
+    color: '#8a05be',
+  },
+  linkXs: {
+    textDecoration: 'underline',
+    color: '#8a05be',
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '15px',
+  },
+  textXs: {
+    paddingTop: '50px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
+    paddingBottom: '10px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+});
 
-export default {styles}
+export default {styles};
