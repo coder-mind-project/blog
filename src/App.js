dc7e7bc0dfdd89273dd01f41ba0b82aa157282c5
Allan Wanerley Alves <awallan259@gmail.com>
2020-12-13 20:35:13 -0300

    chore: Added standard styles from material ui theme and Cleaned up index.css/App.css

--- src/App.js
+++ src/App.js
@@ -15,29 +15,36 @@ import NotFound from './pages/NotFound.jsx';
 
 import ScrollToTop from './config/ScrollToTop';
 
-import './App.css';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import {standard} from './config/themes';
+
+import './index.css';
 import './config/axios';
 
 
 const App = () => {
+  const theme = standard();
+
   return (
-    <Box className="App">
-      <Router>
-        <Menu />
-        <ScrollToTop>
-          <Switch>
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/artigos" exact component={ArticlesList}></Route>
-            <Route path="/artigos/:resource" exact component={Article}></Route>
-            <Route path="/privacidade" exact component={Privacity}></Route>
-            <Route path="/faq" exact component={Faq}></Route>
-            <Route path="/sobre" exact component={About}></Route>
-            <Route component={NotFound}></Route>
-          </Switch>
-        </ScrollToTop>
-        <Footer />
-      </Router>
-    </Box>
+    <MuiThemeProvider theme={theme}>
+      <Box className="App">
+        <Router>
+          <Menu />
+          <ScrollToTop>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/artigos" exact component={ArticlesList} />
+              <Route path="/artigos/:resource" exact component={Article} />
+              <Route path="/privacidade" exact component={Privacity} />
+              <Route path="/faq" exact component={Faq} />
+              <Route path="/sobre" exact component={About} />
+              <Route component={NotFound} />
+            </Switch>
+          </ScrollToTop>
+          <Footer />
+        </Router>
+      </Box>
+    </MuiThemeProvider>
   );
 };
 
