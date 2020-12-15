2ac5ff74db53baf8f80f21e5478ceb3cd7eb7deb
Allan Wanerley Alves <awallan259@gmail.com>
2020-12-15 00:19:44 -0300

    refactor: Refactored search article input on Home page and added better responsivity

---
+++ src/components/Divider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box, Divider} from '@material-ui/core';
+
+const CustomDivider = () => {
+  return (
+    <Box width="100%" my={2}>
+      <Divider />
+    </Box>
+  );
+};
+
+export default CustomDivider;
