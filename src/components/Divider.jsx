9569e73878cb650936253870022f97b07e1dedc6
Allan Wanerley Alves <awallan259@gmail.com>
2020-12-20 10:11:11 -0300

    feat: Added prop margin y-Axis

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import {Box, Divider} from '@material-ui/core';
 
-const CustomDivider = () => {
+const CustomDivider = (props) => {
+  const {my} = props;
   return (
-    <Box width="100%" my={2}>
+    <Box width="100%" my={my}>
       <Divider />
     </Box>
   );
+};
+
+CustomDivider.propTypes = {
+  my: PropTypes.number,
+};
+
+CustomDivider.defaultProps = {
+  my: 2,
 };
 
 export default CustomDivider;
