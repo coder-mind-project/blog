a04c5357e082f312fffabeaa171bc679227156f8
Allan Wanerley Alves <awallan259@gmail.com>
2020-12-26 10:42:49 -0300

    chore AB#40: Refactored Page Not Found component

--- src/pages/styles/index.js
+++ src/pages/styles/index.js
@@ -1,6 +1,31 @@
-import {Grid, styled} from '@material-ui/core';
+import {Grid, styled, Icon, Box} from '@material-ui/core';
+
+import {devices} from '../../config/constants/devices';
 
 export const Container = styled(Grid)({
   padding: '15vh 10vw',
   minHeight: '100vh',
+});
+
+export const NotFoundContainer = styled(Grid)({
+  padding: '15vh 10vw',
+  minHeight: '80vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+});
+
+export const NotFoundIcon = styled(Icon)({
+  fontSize: '5rem',
+  margin: '0 10px',
+});
+
+export const NotFoundContent = styled(Box)({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  [devices.mobileExtraLarge]: {
+    flexDirection: 'column',
+  },
 });
