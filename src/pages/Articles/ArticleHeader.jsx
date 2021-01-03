3d2ad694855038bbf770d4e21f595187fdf0121a
Allan Wanerley Alves <awallan259@gmail.com>
2021-01-03 01:39:48 -0300

    chore AB#49: Enhanced publishedAt information on template

--- src/pages/Articles/ArticleHeader.jsx
+++ src/pages/Articles/ArticleHeader.jsx
@@ -7,12 +7,20 @@ import {faTag, faTags} from '@fortawesome/free-solid-svg-icons';
 
 import {articleType} from '../../types';
 
-import {ArticleHudHeader} from './styles';
+import {ArticleHudHeader, ArticleTitleArea, ArticleDescriptionArea} from './styles';
 
 const ArticleHeader = (props) => {
   const {article} = props;
   return (
     <ArticleHudHeader>
+      <Box>
+        <ArticleTitleArea>
+          <Typography component="h1" variant="h3">{article.title}</Typography>
+        </ArticleTitleArea>
+        <ArticleDescriptionArea>
+          <Typography component="h2" variant="h5">{article.description}</Typography>
+        </ArticleDescriptionArea>
+      </Box>
       <Box display="flex" justifyContent="center" alignItems="center" mx={1} width="100%">
         <Box mr={1} display="flex" alignItems="center">
           <Avatar
@@ -40,7 +48,7 @@ const ArticleHeader = (props) => {
         }
       </Box>
       <Box display="flex" alignItems="center" justifyContent="flex-end" width="100%">
-        <Typography component="span" variant="body1">Publicado em: {`${article.publishedAt}`}</Typography>
+        <Typography component="span" variant="body1">Publicado em: {`${article.publishedAt && article.publishedAt.toLocaleDateString()}`}</Typography>
       </Box>
       <Box width="100%" mt={3} mb={1}>
         <Divider/>
